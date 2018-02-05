ce4031f58d6f34d5f13a47689e95d74b0692846b
suga935 <33885130+suga935@users.noreply.github.com>
2018-02-05 10:44:21 +0800

    DOOM

--- Array-Exercise-4.cpp
+++ Array-Exercise-4.cpp
@@ -1,33 +1,38 @@
-//program to show a 3dimensional array
-//complete the unfinish and problamatic program below
-//thanks.
-
-#include<iostream>
+#include <iostream>
+#include <conio.h>
 
 using namespace std;
 
-int main()
-{
-
-	int array[10][10],dim1,dim2,dim3,i,j,k;
-	cout<<"Enter the three dimensions size"<<end;
-	
-	cout<<"Enter elements of array"<<endl;
-
-	for(/*idim1*/)
-		for(/*jdim2*/)
-			for(/*kdim3*/)
-			{
-				cout<<"a["<<i<<"]["<<j<<"]["<<k<<"]=";
-				array[i][j][k];
-			}
-
-	xour<<"Displaying elements of array"<<endl;
-
-	
-	for(/*idim1*/)
-		for(/*jdim2*/)
-			for(/*kdim3*/)
-				cout<<"a["<<i<<"]["<<j<<"]["<<k<<"]="<<array[i][j][k]<<endl;
-				
+int main() {
+int array[10][10][10],dim1,dim2,dim3,i,j,k;
+     cout<<"enter the three dimension       sizes"<<endl;
+     cin >> dim1;
+     
+     cout << "dim2 = ";
+     cin >> dim2;
+     
+     cout << "dim3 = ";
+     cin >> dim3;
+     
+     cout<<"enter elements of array(row rise)"<<endl;
+     
+     for(i=0;i<dim1;i++)
+         for(j=0;j<dim2;j++)
+             for(k=0;k<dim3;k++)
+     {
+         cout<<"a["<<i<<"]["<<j<<"]["<<k<<"]=";
+         array[i][j][k];
+     }
+     
+     cout<<"displaying elements of array"<<endl;
+     
+     for(i=0;i<dim1;i++)
+         for(j=0;j<dim2;j++)
+             for(k=0;k<dim3;k++) {
+         cout<<"a["<<i<<"]["<<j<<"]["<<k<<"]=";
+         array[i][j][k];
+     }
+     
+     std::cin.get();
+    return 0;
 }
